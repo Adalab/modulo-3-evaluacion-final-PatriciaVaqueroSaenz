7b426f8d4caee3a9ca448409210b50c84edd4510
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-15 19:56:33 +0200

    add folders

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+const App = () => {
+  // state
+
+  // effects
+
+  // event handlers
+
+  // render
+
+  return (
+    <>
+      <h1 className="title">Rick y Morty</h1>
+    </>
+  );
+};
+
+export default App;

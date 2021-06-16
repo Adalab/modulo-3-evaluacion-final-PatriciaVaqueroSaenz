c00c57bc422c71944c489f763fc86adfe50b9dd9
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-16 13:16:24 +0200

    add specie status

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import human from "../images/human.png";
+import alien from "../images/alien.png";
+import unknown from "../images/unknown.png";
 
 const Character = props => {
 
+  const getSpecie = () => {
+    if (props.character.species === "Human") {
+      return human;
+    } else if (props.character.species === "Alien") {
+      return alien;
+    } else {
+      return unknown;
+    }
+  };
   return (
     <Link className='link' to={`/character/${props.character.id}`}>
       <article className="card">
@@ -14,7 +26,12 @@ const Character = props => {
         />
         <h4 className="card__title">{props.character.name}</h4>
         <p className="card__description">
-          {props.character.species}
+          <img
+              className="card-detail__specie"
+              src={getSpecie()}
+              alt={props.character.species}
+              title={props.character.species}
+            />
         </p>
       </article>
       </Link>

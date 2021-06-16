1ee40673ec2501f116284cf83e1fd4a1e29a9636
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-16 11:52:16 +0200

    add scss to character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import exit from '../images/exit.png';
 
 const CharacterDetail = props => {
   return (
     <div className="card-detail">
-      <Link className="linkGoBack" to="/">Go back</Link>
+      <Link className="linkGoBack" to="/"><img className="exitIcon" src={exit} alt="exit" /></Link>
       <header>
         <h2 className="card-detail__title">{props.character.name}</h2>
       </header>
-      <section>
+      <section className="card-detail__content">
         <img className="card-detail__img" src={props.character.image} alt={props.character.name} />
-        <h4 className="card-detail__name">{props.character.name}</h4>
         <ul className="card-detail__info">
           <li>Specie: {props.character.species}</li>
-          <li>Planet: {props.character.origin.name}</li>
+          <li>Planet: {props.character.origin}</li>
           <li>Status: {props.character.status}</li>
           {/* <li>Episodes: {props.character.country}</li> */}
         </ul>

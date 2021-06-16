44811fe5d2301c0377c99a818036da6da11b1ad3
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-16 10:13:36 +0200

    add character not found component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,29 +1,22 @@
 import React from 'react';
 import Character from './Character';
-import photo from '../images/notFound.png'
+import CharacterNotFound from './CharacterNotFound';
 
 const CharacterList = props => {
   //si buscamos un personaje que no existe
   if (props.characters.length === 0) {
     return (
-      <div className='container-notFound'>
-        <h2 className='container-notFound-title'>Holy shit, human!</h2>
-        <img className='container-notFound__img' src={photo} alt='character not found' />
-        <div className='container-notFound__text'>
-          <p>This character doesn't exist.</p>
-          <p>Try again with other name.</p>
-        </div>
-      </div>
+      <CharacterNotFound/>
     );
   //si el personaje(s) existen los pintamos
   } else {
-    const charElements = props.characters.map(character => {
-      return (
-        <li key={character.id} className='card-item'>
-          <Character character={character} />
-        </li>
-      );
-    });
+      const charElements = props.characters.map(character => {
+        return (
+          <li key={character.id} className='card-item'>
+            <Character character={character} />
+          </li>
+        );
+      });
 
     return (
     <section className='container-cards'>
@@ -31,6 +24,6 @@ const CharacterList = props => {
     </section>
     );
   }
-    };
+};
 
 export default CharacterList;
---
+++ src/components/CharacterNotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import photo from '../images/notFound.png'
+
+const CharacterNotFound = () => {
+
+  return (
+    <div className='container-notFound'>
+    <h2 className='container-notFound-title'>Holy shit, human!</h2>
+    <img className='container-notFound__img' src={photo} alt='character not found' />
+    <div className='container-notFound__text'>
+      <p>This character doesn't exist.</p>
+      <p>Try again with other name.</p>
+    </div>
+  </div>
+  );
+};
+
+export default CharacterNotFound;

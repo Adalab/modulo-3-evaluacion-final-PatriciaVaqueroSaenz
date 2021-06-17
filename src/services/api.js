7b07113fdc3032cf5b625c11e3ac0d6ab14c788f
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-17 09:08:31 +0200

    add format to services

--- src/services/api.js
+++ src/services/api.js
@@ -1,30 +1,26 @@
 const getApiData = () => {
-    return (
-      fetch('//rickandmortyapi.com/api/character')
+  return (
+    fetch("//rickandmortyapi.com/api/character")
       //https:////rickandmortyapi.com/api/character
       //https://raw.githubusercontent.com/Adalab/rick-y-morty/master/data/rick-y-morty.json
-        
-        .then(response => response.json())
-        .then(data => {
-          const cleanData = data.results.map(character => {
-            return {
-              id: character.id,
-              gender: character.gender,
-              image: character.image,
-              name: character.name,
-              species: character.species,
-              origin: character.origin.name,
-              status: character.status,
-              episode: character.episode
-            };
-            
-          });
-          return cleanData;
-          
-        })
-        
-    );
-  };
-  
-  export default getApiData;
-  
+
+      .then((response) => response.json())
+      .then((data) => {
+        const cleanData = data.results.map((character) => {
+          return {
+            id: character.id,
+            gender: character.gender,
+            image: character.image,
+            name: character.name,
+            species: character.species,
+            origin: character.origin.name,
+            status: character.status,
+            episode: character.episode,
+          };
+        });
+        return cleanData;
+      })
+  );
+};
+
+export default getApiData;

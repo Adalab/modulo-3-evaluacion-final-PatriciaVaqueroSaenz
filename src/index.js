6f35719d99e51fcdfd2b1db0ac6f79447bba7f2d
PatriciaVaqueroSaenz <vaquerosaenzpatricia@gmail.com>
2021-06-17 09:15:05 +0200

    add format to index js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
-import './stylesheets/index.scss';
-import App from './components/App';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter } from "react-router-dom";
+import "./stylesheets/index.scss";
+import App from "./components/App";
 
 ReactDOM.render(
   <HashRouter>
     <App />
   </HashRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
